e651ad47f38861a01a50bd03bd54ab0e6c341984
Furqan Attar <furqanattar96@gmail.com>
2018-03-06 12:25:30 -0600

    Initial Commit

---
+++ Program2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program2
+//
+//  Created by Furqan Attar on 3/6/18.
+//  Copyright © 2018 Furqan Attar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
